42f29ab6bf3759d087084ecd360680ecb77f6bef
titli1981 <madhumishra084@gmailcom>
2016-08-09 15:31:49 -0400

    Added the solution of four problems

---
+++ exercise2.rb
@@ -0,0 +1,18 @@
+# calculate a good tip for a 55 dollar meal?
+meal = 55
+tip = 0.13
+ puts " #{meal*tip}"
+
+# Try adding a string and an integer with the + operator. What happens? Find a way to convert the integer into a string first and use puts to print the result.
+
+ puts "Madhu " + "4"
+ puts "madhu " + 4.to_s
+
+ #Try outputting the result of 45628 multiplied by 7839 in a sentence by using string interpolation.
+
+puts "I am #{45628 * 7839} year old"
+
+# What's the value of the expression (10 < 20 && 30 < 20) || !(10 == 11)? Try figuring it out on your own before typing it in.
+
+puts (10 < 20 && 30 < 20) || !(10 ==11)
+# =>true
