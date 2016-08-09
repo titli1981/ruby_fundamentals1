5d96ab75c76fe7e633a73bd8f311116198ccfb20
titli1981 <madhumishra084@gmailcom>
2016-08-09 14:46:23 -0400

    added escape sequence

--- exercise1.rb
+++ exercise1.rb
@@ -4,3 +4,13 @@
  puts 2 + 3
 
  #puts 2 puts 3 puts  2 + 3
+
+ #escape sequence - The combination of the backslash followed by the single quote is called an escape sequence.
+
+ puts "Betty's pie shop"
+ puts 'Betty\'s pie shop'
+ puts " Ada Lovelace lived for #{ 1852 - 1815} years."
+ puts "Hello\t\tworld"
+ puts " Hello\b\b\b\b\bGoodbye world"
+ puts " Hello\rSrart over world"
+ puts " 1. Hello\n2. world"
