fe2dc51cdde6cf7dbc6623bf38e0870adbe6608a
titli1981 <madhumishra084@gmailcom>
2016-08-09 16:22:24 -0400

    calculated the year

--- exercise3.rb
+++ exercise3.rb
@@ -4,3 +4,10 @@ puts "What is your name?"
 name = gets.chomp
 
 puts "Hi #{name}"
+
+puts "How old are you?"
+age = gets.chomp.to_i
+
+puts "You are #{age} year old"
+
+puts "you are  born in #{2016 - age}"
